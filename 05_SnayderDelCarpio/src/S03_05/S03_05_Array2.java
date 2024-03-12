9972dfd586a7b56af3682c6173723e7a395b64d3
SnayderDc <snayderdel.carpio.c@vallegrande.edu.pe>
2024-03-11 22:58:04 -0500

    array2

--- 05_SnayderDelCarpio/src/S03_05/S03_05_Array2.java
+++ 05_SnayderDelCarpio/src/S03_05/S03_05_Array2.java
@@ -15,7 +15,7 @@ public class S03_05_Array2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    int [][] numeros2 = {{10, 20},{50, 40}};
+    int [][] numeros2 = {{10, 20},{50, 60}};
        
         System.out.println(numeros2 [1][0]);
     }
